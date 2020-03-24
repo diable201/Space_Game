44f6089276645b8540c43383f2f4f17c95eb7dfc
Sanzhar <diable201@protonmail.com>
2020-03-24 22:41:55 +0600

    UPD
    Changed position of classes. Removed sprites of player. Added texture for player laser.

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 img_dir = path.join(path.dirname(__file__), 'Textures')
 enemy_img = pygame.image.load('Textures/Enemy/enemy.png').convert_alpha()
 enemy_bullet_img = pygame.image.load('Textures/Enemy/laser.png').convert()
+player_bullet_img = pygame.image.load('Textures/Player/laser.png').convert()
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -22,57 +23,6 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-
-
-class EnemyShip(pygame.sprite.Sprite):
-    def __init__(self, enemy_image, bullet_image, sprites_list, bullet_list):
-        pygame.sprite.Sprite.__init__(self)
-        # Sprites
-        self.image = pygame.transform.scale(enemy_image, (60, 60))
-        self.rect = self.image.get_rect()
-        self.sprites = sprites_list
-        # Start position
-        self.rect.x = random.randint(20, WIDTH - 20)
-        self.rect.y = random.randrange(-140, -30)
-        self.speedy = random.randrange(2, 7)
-        self.speedx = random.randrange(-1, 1)
-
-        # Bullet settings
-        self.bullet_image = bullet_image
-        self.bullets = bullet_list
-        self.shoot_delay = random.randint(1200, 1500)
-        self.last_shot = pygame.time.get_ticks()
-        self.num_of_shots = 1
-
-        # Kamikaze
-        # self.speedy = 30
-
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        if self.rect.top > HEIGHT + 15 or self.rect.left < -20 or self.rect.right > WIDTH + 20:
-            self.rect.x = random.randrange(WIDTH - self.rect.width)
-            self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(2, 7)
-        # Shoot
-        for shoot in range(self.num_of_shots):
-            self.shoot()
-
-    # def update(self):
-    #     # self.rect.x += self.speedx
-    #     # self.rect.y += self.speedy
-    #     # if self.rect.top > HEIGHT or self.rect.left < -10 or self.rect.right > WIDTH:
-    #     #     self.rect.x = random.randrange(90, WIDTH - 90)
-    #     #     self.rect.y = random.randrange(-150, -20)
-    #     #     self.speedy = random.randrange(1, 8)
-
-    def shoot(self):
-        current_time = pygame.time.get_ticks()
-        if current_time - self.last_shot > self.shoot_delay:
-            self.last_shot = current_time
-            bullet = EnemyBullet(self.bullet_image, self.rect.centerx, self.rect.bottom)
-            self.sprites.add(bullet)
-            self.bullets.add(bullet)
 
 
 class Player(pygame.sprite.Sprite):
@@ -119,13 +69,72 @@ class Player(pygame.sprite.Sprite):
         bullets.add(bullet)
 
 
+class EnemyShip(pygame.sprite.Sprite):
+    def __init__(self, enemy_image, bullet_image, sprites_list, bullet_list):
+        pygame.sprite.Sprite.__init__(self)
+        # Sprites
+        self.image = pygame.transform.scale(enemy_image, (60, 60))
+        self.rect = self.image.get_rect()
+        self.sprites = sprites_list
+        # Start position
+        self.rect.x = random.randint(20, WIDTH - 20)
+        self.rect.y = random.randrange(-140, -30)
+        self.speedy = random.randrange(2, 7)
+        self.speedx = random.randrange(-1, 1)
+
+        # Bullet settings
+        self.bullet_image = bullet_image
+        self.bullets = bullet_list
+        self.shoot_delay = random.randint(1200, 1500)
+        self.last_shot = pygame.time.get_ticks()
+        self.num_of_shots = 1
+
+        # Kamikaze
+        # self.speedy = 30
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 15 or self.rect.left < -20 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(2, 7)
+        # Shoot
+        for shoot in range(self.num_of_shots):
+            self.shoot()
+
+    def shoot(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_shot > self.shoot_delay:
+            self.last_shot = current_time
+            bullet = EnemyBullet(self.bullet_image, self.rect.centerx, self.rect.bottom)
+            self.sprites.add(bullet)
+            self.bullets.add(bullet)
+
+
+class PlayerBullet(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_bullet_img, (8, 23))
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10  # отрицательное для движения вверх
+
+    def update(self):
+        self.rect.y += self.speedy
+        # убить, если он заходит за верхнюю часть экрана
+        if self.rect.bottom < 0:
+            self.kill()
+
+
 class EnemyBullet(pygame.sprite.Sprite):
 
     def __init__(self, bullet_image, x, y):
         pygame.sprite.Sprite.__init__(self)
         # scale bullet size
         self.image = pygame.transform.scale(bullet_image, (8, 23))
-        self.image.set_colorkey(RED)
         self.rect = self.image.get_rect()
         # bullet position is according the player position
         self.rect.centerx = x
@@ -137,24 +146,6 @@ class EnemyBullet(pygame.sprite.Sprite):
         self.rect.y += self.speedy
         # if bullet goes off bottom of window, destroy it
         if self.rect.bottom > HEIGHT:
-            self.kill()
-
-
-class PlayerBullet(pygame.sprite.Sprite):
-
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(YELLOW)
-        self.rect = self.image.get_rect()
-        self.rect.bottom = y
-        self.rect.centerx = x
-        self.speedy = -10  # отрицательное для движения вверх
-
-    def update(self):
-        self.rect.y += self.speedy
-        # убить, если он заходит за верхнюю часть экрана
-        if self.rect.bottom < 0:
             self.kill()
 
 
